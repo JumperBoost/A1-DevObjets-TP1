4f5f67530d48a24ab82b37da11b48ed6327bd1fb
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-01-16 09:31:03 +0000

    Mise à jour Exercice 1

--- Exercice1/HelloWorld.java
+++ Exercice1/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Salut le monde");
+        System.out.println("Je suis une autre version modifiée du code de l'exo 1");
     }
 }
